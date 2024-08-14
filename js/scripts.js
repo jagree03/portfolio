baf29fa8bdc5627433a1314aef8b345fdff0a041
Jagjeet Reehal <100100736+jagree03@users.noreply.github.com>
2024-08-14 14:45:45 +0100

    Made a start on my portfolio site by adding a simple structure with some styling and JS code, along with adding one of my projects.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+///////////////////
+// Accessing elements in the DOM
+///////////////////
+const traffic_simulatorImage = document.getElementById('traffic_simulator');
+
+
+
+///////////////////
+// Event listeners
+///////////////////
+traffic_simulatorImage.addEventListener('mouseover', ev => {
+    traffic_simulatorImage.src = "img/trafficsim_01.gif";
+    traffic_simulatorImage.style.width = "700px";
+    traffic_simulatorImage.style.height = "auto";
+})
+
+traffic_simulatorImage.addEventListener('mouseout', ev => {
+    traffic_simulatorImage.src = "img/trafficsim_00.png";
+    traffic_simulatorImage.style.width = "700px";
+    traffic_simulatorImage.style.height = "auto";
+})
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Jagjeet Reehal's Portfolio Page
+
+Source files of my portfolio showcasing all my projects in brief.
+
+Created using HTML, CSS and JavaScript.
